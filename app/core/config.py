83ef8159c73b1da5b56243225142995acf80c118
sumitsingh <singh91308@gmail.com>
2026-01-08 19:29:14 +0530

    Update core configuration for environment-based settings

--- app/core/config.py
+++ app/core/config.py
@@ -1,19 +1,13 @@
 from pydantic_settings import BaseSettings
 from pathlib import Path
 
-
-BASE_DIR = Path(__file__).resolve().parent.parent
-
-
 class Settings(BaseSettings):
     GEMINI_API_KEY: str
-    #OPENAI_API_KEY: str
-    DATABASE_URL: str | None = None
-    SECRET_KEY: str | None = None
-    algorithm:str="HS256"
+    DATABASE_URL: str
+    SECRET_KEY: str
+    ALGORITHM: str = "HS256"
 
     class Config:
-        env_file = BASE_DIR.parent / ".env"
-
+        env_file = ".env"
 
 settings = Settings()
